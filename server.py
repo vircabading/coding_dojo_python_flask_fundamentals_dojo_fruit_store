e37f86a40ab727a34d346b82696350a93a907842
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-28 23:50:09 -0700

    add record form variables at server

--- server.py
+++ server.py
@@ -1,19 +1,28 @@
 from flask import Flask, render_template, request, redirect
+from werkzeug.datastructures import RequestCacheControl
 app = Flask(__name__)  
 
 @app.route('/')         
 def index():
     return render_template("index.html")
 
-@app.route('/checkout')         
-def checkout():
-    # print(request.form)
-    return render_template("checkout.html")
-
-# @app.route('/checkout', methods=['POST'])         
+# @app.route('/checkout')         
 # def checkout():
-#     print(request.form)
+#     # print(request.form)
 #     return render_template("checkout.html")
+
+@app.route('/checkout', methods=['POST'])         
+def checkout():
+    print(request.form)
+    strawberry_num = request.form['strawberry']                         # Retrieve variables from the form
+    raspberry_num = request.form['raspberry']
+    apple_num = request.form['apple']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    student_id = request.form['student_id']
+    print(f"straberry num: {strawberry_num} :: raspberry num: {raspberry_num} :: apple num: {apple_num}")
+    print(f"first name: {first_name} :: last name: {last_name} :: student id: {student_id}")
+    return render_template("checkout.html", strawberry_num = strawberry_num , raspberry_num = raspberry_num, apple_num = apple_num, first_name= first_name, last_name = last_name, student_id = student_id)
 
 @app.route('/fruits')         
 def fruits():
