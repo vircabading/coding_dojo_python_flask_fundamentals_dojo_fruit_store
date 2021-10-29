db8e624c12381099c92813e2adc07063e233a0f9
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-29 14:00:27 -0700

    add session

--- server.py
+++ server.py
@@ -1,27 +1,31 @@
 import datetime
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from werkzeug.datastructures import RequestCacheControl
 
-app = Flask(__name__)  
+app = Flask(__name__)
+app.secret_key = "TiYSKDNRitA!"                                                     # This is Your Secret Key Do Not Reveal it to Anyone!
 
 @app.route('/')         
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
-def checkout():
+@app.route('/post_checkout', methods=['POST'])
+def post_checkout():
     print(request.form)
-    strawberry_num = request.form['strawberry']                         # Retrieve variables from the form
-    raspberry_num = request.form['raspberry']
-    apple_num = request.form['apple']
-    total_num_ordered = int(strawberry_num) + int(raspberry_num) + int(apple_num)
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    student_id = request.form['student_id']
+    session['strawberry_num'] = request.form['strawberry']                          # Retrieve variables from the form
+    session['raspberry_num'] = request.form['raspberry']                            #     and store the vars as in a session
+    session['apple_num'] = request.form['apple']
+    session['first_name'] = request.form['first_name']
+    session['last_name'] = request.form['last_name']
+    session['student_id'] = request.form['student_id']
+    return redirect ("/checkout")
+    
+
+@app.route('/checkout')         
+def checkout():
     current_date_time = datetime.datetime.now()
-    # print(f"straberry num: {strawberry_num} :: raspberry num: {raspberry_num} :: apple num: {apple_num}")
-    # print(f"first name: {first_name} :: last name: {last_name} :: student id: {student_id}")
-    return render_template("checkout.html", strawberry_num = strawberry_num , raspberry_num = raspberry_num, apple_num = apple_num, total_num_ordered= total_num_ordered, first_name= first_name, last_name = last_name, student_id = student_id, current_date_time = current_date_time)
+    total_num_ordered = int(session['strawberry_num']) + int(session['raspberry_num']) + int(session['apple_num'])
+    return render_template("checkout.html", total_num_ordered= total_num_ordered, current_date_time= current_date_time)
 
 @app.route('/fruits')         
 def fruits():
