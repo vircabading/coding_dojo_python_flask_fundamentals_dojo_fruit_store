86ed2b5161d43131e4c56c5cdfc7e61cb16d8388
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-29 16:01:38 -0700

    add session records a dictionary

--- server.py
+++ server.py
@@ -12,19 +12,23 @@ def index():
 @app.route('/post_checkout', methods=['POST'])                                      # Page to handle data from submitting the checkout form
 def post_checkout():
     print(request.form)
-    session['strawberry_num'] = request.form['strawberry']                          # Retrieve variables from the form
-    session['raspberry_num'] = request.form['raspberry']                            #     and store the vars as in a session
-    session['apple_num'] = request.form['apple']
-    session['first_name'] = request.form['first_name']
-    session['last_name'] = request.form['last_name']
-    session['student_id'] = request.form['student_id']
+    # session['strawberry_num'] = request.form['strawberry']                          # Retrieve variables from the form
+    # session['raspberry_num'] = request.form['raspberry']                            #     and store the vars as in a session
+    # session['apple_num'] = request.form['apple']
+    # session['first_name'] = request.form['first_name']
+    # session['last_name'] = request.form['last_name']
+    # session['student_id'] = request.form['student_id']
+    session['fruit_store'] = {
+        **request.form
+    }
     return redirect ("/checkout")
     
 
 @app.route('/checkout')                                                             # Page for showing the checkout info
 def checkout():
     current_date_time = datetime.datetime.now()
-    total_num_ordered = int(session['strawberry_num']) + int(session['raspberry_num']) + int(session['apple_num'])
+    total_num_ordered = int(session['fruit_store']['strawberry']) + int(session['fruit_store']['raspberry']) + int(session['fruit_store']['apple'])
+
     return render_template("checkout.html", total_num_ordered= total_num_ordered, current_date_time= current_date_time)
 
 @app.route('/fruits')                                                               # display the fruit pictures
